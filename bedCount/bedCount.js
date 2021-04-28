001abf6b8118be3c343d45194fe389e4069abe25
knk4748 <kchawla_be19@thapar.edu>
2021-04-28 08:19:29 +0530

    minor correction

--- bedCount/bedCount.js
+++ bedCount/bedCount.js
@@ -3,7 +3,7 @@
   
       function attachHospital(hosp){
         console.log(hosp)
-        htmlFormatted  = " <div class= \"container info-box\">  <div class=\"hospital-info\"> <h1>"+ hosp.name +"</h1><p class=\"address\">Sangrur Road, New Lal Bagh Colony, Patiala, Punjab 147001</p><button type=\"button\" class=\"btn btn-primary\"  onclick=\"location.href = 'https://www.google.com/search?q="+hosp.name + " ';\" >FIND US</button></div> <div class=\"bed-box\">       <p class=\"px-3 about-content text-justify\">L2 Beds Available:"+ hosp.l2_occupied+" out of "+ hosp.l2_beds+ "</p> <p class=\"px-3 about-content text-justify\">L3 Available:"+ hosp.l3_occupied+" out of "+ hosp.l3_beds+"</p>     </div>       </div> "
+        htmlFormatted  = " <div class= \"container info-box\">  <div class=\"hospital-info\"> <h1>"+ hosp.name +"</h1><p class=\"address\"></p><button type=\"button\" class=\"btn btn-primary\"  onclick=\"location.href = 'https://www.google.com/search?q="+hosp.name + " ';\" >FIND US</button></div> <div class=\"bed-box\">       <p class=\"px-3 about-content text-justify\">L2 Beds Available:"+ hosp.l2_occupied+" out of "+ hosp.l2_beds+ "</p> <p class=\"px-3 about-content text-justify\">L3 Available:"+ hosp.l3_occupied+" out of "+ hosp.l3_beds+"</p>     </div>       </div> "
         $("#listView").prepend(htmlFormatted)
       }
   
